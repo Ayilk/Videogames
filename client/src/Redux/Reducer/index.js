42bcdaa32e6ba088193b9e0f219cdaa8ae966f80
Ayilk <srangel.h92@gmail.com>
2022-07-02 15:35:26 -0500

    Empezando conexion con el back, ruta get videogames

--- client/src/Redux/Reducer/index.js
+++ client/src/Redux/Reducer/index.js
@@ -1,5 +1,20 @@
-function rootReducer(){
+import { GET_GAMES } from "../Actions";
 
+
+const initialState = { //Creamos el estado inicial
+    videogames: []
+}
+
+function rootReducer(state = initialState, action){
+    switch(action.type){
+        case GET_GAMES:
+            return {
+                ...state,
+                videogames: action.payload
+            }
+        default:
+            return state;    
+    }
 }
 
 export default rootReducer;
